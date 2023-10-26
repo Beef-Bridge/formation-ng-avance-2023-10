a0664d51cd725b4c849430a84f657e3174d68c81
Nikoquattro <nikoquattro@gmail.com>
2023-10-26 11:36:04 +0200

    TD : remettre à plat la logique d’écriture d’un observable

--- webApp/src/app/webApp/formation/rxjs/composants-routing/observables/observables.component.ts
+++ webApp/src/app/webApp/formation/rxjs/composants-routing/observables/observables.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Observable, Subscription, from } from 'rxjs';
 
 @Component({
   selector: 'app-observables',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./observables.component.scss']
 })
 export class ObservablesComponent {
+  // ----------------------------------------------------
+  public title: string = '';
+  public temps: number = 0;
 
+  // utilse l'élément template 'formations1'
+  @ViewChild('formations1') eltUl1!: ElementRef<HTMLElement>;
+  @ViewChild('formations2') eltUl2!: ElementRef<HTMLElement>;
+  @ViewChild('texte') eltPrenom!: ElementRef<HTMLElement>;
+  @ViewChild('stop') eltStop!: ElementRef<HTMLElement>;
+
+  private subscription: Subscription = new Subscription();
+  private subscriptionTime: Subscription = new Subscription();
+  public infos$: Observable<string> = {} as Observable<string>;
+
+  // ----------------------------------------------------
+  public createObservable1: any = () => {
+    this.eltUl1.nativeElement.innerHTML = '';
+
+    const formationsArray: string[] = ['NG16', 'REACT 17', 'PWA', 'XML', 'JSON', 'PWA', 'TS'];
+
+    // création de l'observable
+    const formation$:Observable<string> = from(formationsArray);
+
+    console.log(formation$);
+
+    formation$
+      .pipe(
+        // permet d'enchainer les opérateurs
+      )
+      .subscribe({
+        // la notion d'observers NEXT ERROR COMPLETE
+        next:
+        (formation:string) => {
+          console.log(formation);
+        },
+        error:
+        (e:any) => console.log(e)
+        ,
+        complete:
+        () => console.warn('Complete')
+      })
+  }
 }
