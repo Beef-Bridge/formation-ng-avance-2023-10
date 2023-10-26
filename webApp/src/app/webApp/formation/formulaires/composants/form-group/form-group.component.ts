c18c4da6e33925a38dcaf0ab23c2b48f410ea379
Nikoquattro <nikoquattro@gmail.com>
2023-10-26 17:01:52 +0200

    TD : form-group (fin de J2)

--- webApp/src/app/webApp/formation/formulaires/composants/form-group/form-group.component.ts
+++ webApp/src/app/webApp/formation/formulaires/composants/form-group/form-group.component.ts
@@ -16,8 +16,43 @@ export class FormGroupComponent {
   constructor(private _post: PostService) {
 
     this.monForm = new FormGroup({
-      prenom: new FormControl<string | null>(null)
+      // déclaration de tous les controls de ce formulaire (formGroup)
+      prenom: new FormControl<string | null>(
+        //val par défaut
+        null,
+        // Validators
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(10)
+        ]),
+      nom: new FormControl<string | null>(null),
+      // email :
+      email: new FormControl<string | null>(
+        null,
+        [
+          // options
+          // Validators.email,
+          // ^:commence par
+          // $: finit par
+          // [a-z] : toutes les lettres en minuscule de a à z
+          // [a-zA-Z0-9] : toutes les lettres min et maj + les chiffres
+          // ? : on répéte 0 ou 1 fois
+          // + : 1 ou plusieurs fois
+          // * : 0 ou plusieurs fois
+          // {val min , val max } : pour répéter
+          Validators.pattern('^[a-z0-9._-]+@[a-z0-9.-]+\\.[a-z]{2,}$'),
+          Validators.required
+        ]
+      ),
+           // ******************************
+      adresse: new FormGroup({
+        rue: new FormControl<string | null>(null),
+        ville: new FormControl<string | null>(null, Validators.required),
+        cp: new FormControl<string | null>(null),
+      }),
     })
+    
   }
 
 }
