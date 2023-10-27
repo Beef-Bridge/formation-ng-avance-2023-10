cbabb311cc9ddd126f47d93917d6b72bd09828a0
Nikoquattro <nikoquattro@gmail.com>
2023-10-27 11:44:03 +0200

    evolution premier test unitaire

--- webApp/src/app/webApp/formation/test-unitaires/services/operations.service.spec.ts
+++ webApp/src/app/webApp/formation/test-unitaires/services/operations.service.spec.ts
@@ -1,5 +1,6 @@
-import { OperationsService } from './operations.service';
-import { WarnService } from './warn.service';
+import { TestBed } from "@angular/core/testing";
+import { OperationsService } from "./operations.service";
+import { WarnService } from "./warn.service";
 
 // Création de l'objet de Test
 describe('Test Injection de dépendances', () => {
@@ -8,22 +9,34 @@ describe('Test Injection de dépendances', () => {
   // ---------------------------------------------------
   let MockWarn: any;
   let operations: any;
-
   // ---------------------------------------------------------------------
   // traitement spécifiques communs (beforeEach - AfterEach - beforeAll)
   // ---------------------------------------------------------------------
   beforeEach(() => {
-    MockWarn = jasmine.createSpyObj('WarnService', [
-      'warnMessage',
-      'logErreur',
-    ]);
-    operations = new OperationsService(MockWarn);
+    MockWarn = jasmine.createSpyObj('WarnService', ['warnMessage','logErreur']);
+    // ---------------------------------------------------------------------
+    // Création d'un Objet complet de test et de simulation : TESTBED
+    // Créons une vraie injection de dépendances avec les providers
+    // ---------------------------------------------------------------------
+    TestBed.configureTestingModule({
+      providers: [
+        // injection de dépendances
+        OperationsService,
+        {
+          provide: WarnService,
+          // on n'utilise pas la méthode de WarnService
+          // mais la méthode du Mock ('warnMessage', 'logErreur')
+          useValue: MockWarn
+        }
+      ]
+    });
+    // --------------------------------------------
+    operations = TestBed.inject(OperationsService);
+    // --------------------------------------------
   });
-
   // ----------------------------
   //   liste des spécifications
   // ----------------------------
-
   it('Ajouter 2 nombres OK ?', () => {
     const resultat = operations.ajouter(10, 5);
     expect(resultat).withContext('--- Erreur Expectation ---').toBe(15);
@@ -34,4 +47,6 @@ describe('Test Injection de dépendances', () => {
     const resultat = operations.soustraire(10, 5);
     expect(resultat).withContext('--- Erreur Expectation ---').toBe(5);
   });
-});
+
+
+})
