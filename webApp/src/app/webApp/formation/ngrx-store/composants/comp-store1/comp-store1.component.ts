3a7a8a6874d3aae5e01814da0b595877cf9b80c9
Nikoquattro <nikoquattro@gmail.com>
2023-10-27 14:36:31 +0200

    TD evolutio 2 : utilisation async

--- webApp/src/app/webApp/formation/ngrx-store/composants/comp-store1/comp-store1.component.ts
+++ webApp/src/app/webApp/formation/ngrx-store/composants/comp-store1/comp-store1.component.ts
@@ -1,39 +1,44 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { changenameAction, initAction } from '../../actions/actions';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-comp-store1',
   templateUrl: './comp-store1.component.html',
-  styleUrls: ['./comp-store1.component.scss']
+  styleUrls: ['./comp-store1.component.scss'],
 })
 export class CompStore1Component {
+  // public actor:any;
+  public actor$ = {} as Observable<any>;
 
-  public actor:any;
-  public isJedi:any;
-
-  constructor(private _store:Store) {}
+  constructor(private _store: Store) {}
 
   ngOnInit() {
     this._store.dispatch(initAction());
+    // -------------------
+    // this._store.select(
+    //   (state:any) => {
+    //     return state.root.actor
+    //   }
+    // ).subscribe(
+    //   (partStateActor) => {
+    //     this.actor = partStateActor
+    //   }
+    // )
 
-    this._store.select(
-      (state:any) => {
-        return state.root.actor
-      }
-    ).subscribe(
-      (partStateActor) => {
-        this.actor = partStateActor
-      }
-    )
+    this.actor$ = this._store.select((state: any) => {
+      return state.root.actor;
+    });
   }
-
+  // -------------------------------
   public changeName = () => {
     this._store.dispatch(
       changenameAction({
-        paramNameActionCOMP:`Luke Skylwalker Jedi - Maître ${Math.round(Math.random() * 10)} dans la lignée des Jedi...`
+        paramNameActionCOMP: `Luke Skylwalker Jedi - Maître ${Math.round(
+          Math.random() * 10
+        )} dans la lignée des Jedi...`,
       })
     );
-  }
-
+  };
 }
